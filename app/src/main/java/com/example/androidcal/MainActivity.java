92b2b236549b5108a65d7bc0340516e64c3bc4a7
Salim Mujawar <salim685@gmail.com>
2019-06-20 17:06:21 +0400

    Implementing Calculator Logic

--- app/src/main/java/com/example/androidcal/MainActivity.java
+++ app/src/main/java/com/example/androidcal/MainActivity.java
@@ -5,13 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtResult;
+
+    String runningNumber = "";
+    String leftBucket;
+    String rightBucket;
+
+    public enum Operation {
+            ADD, SUBTRACT, MULTIPLY, DIVIDE, EQUAL
+    };
+
+    Operation currentOperation;
+
+    int result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
 
         //Take reference of the Number Button elements
         Button btn1 = (Button) findViewById(R.id.btn1);
@@ -35,110 +52,167 @@ public class MainActivity extends AppCompatActivity {
         Button btnClr = (Button) findViewById(R.id.btnClr);
         ImageButton btnEqu = (ImageButton) findViewById(R.id.btnEqu);
 
+        //Set Textview to Zero
+        txtResult.setText("");
+
         //Hook Listener for the buttons
         btn1.setOnClickListener(new View.OnClickListener(){
           @Override
           public void onClick(View v){
-
+            numberPressed(1);
           }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(2);
             }
         });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(3);
             }
         });
 
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(4);
             }
         });
 
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(5);
             }
         });
 
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(6);
             }
         });
 
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(7);
             }
         });
 
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(8);
             }
         });
 
         btn9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(8);
             }
         });
 
         btn0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPressed(0);
             }
         });
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.ADD);
             }
         });
 
         btnSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.SUBTRACT);
             }
         });
 
         btnMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.MULTIPLY);
             }
         });
 
         btnDiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.DIVIDE);
             }
         });
 
         btnClr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                runningNumber = "";
+                leftBucket = "";
+                rightBucket = "";
+                result = 0;
+                currentOperation = null;
+                txtResult.setText("");
             }
         });
+
+        btnEqu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processOperation(Operation.EQUAL);
+            }
+        });
+
+    }
+
+    void processOperation(Operation operation) {
+        if (currentOperation != null) {
+
+            if (runningNumber != null) {
+                rightBucket = runningNumber;
+                runningNumber = "";
+
+                switch (currentOperation){
+                    case ADD:
+                            result = Integer.parseInt(leftBucket) + Integer.parseInt(rightBucket);
+                        break;
+                    case SUBTRACT:
+                            result = Integer.parseInt(leftBucket) - Integer.parseInt(rightBucket);
+                        break;
+                    case MULTIPLY:
+                            result = Integer.parseInt(leftBucket) * Integer.parseInt(rightBucket);
+                        break;
+                    case DIVIDE:
+                            result = Integer.parseInt(leftBucket) / Integer.parseInt(rightBucket);
+                        break;
+                }
+
+                leftBucket = String.valueOf(result);
+                txtResult.setText(leftBucket);
+            }
+        }else {
+            leftBucket = runningNumber;
+            runningNumber = "";
+        }
+
+        currentOperation = operation;
+    }
+
+    /*
+        What number was pressed
+     */
+    public void numberPressed(int number){
+        runningNumber += number;
+        txtResult.setText(runningNumber);
     }
 }
